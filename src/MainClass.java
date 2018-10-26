ae29fbdca5412be7f0064325feb7530d6b012cdc
victorpapa <victorpapa98@gmail.com@>
2018-10-27 00:21:09 +0100

    Java program that implements the finger protocol for getting information about users of a UNIX system, either locally or remotely.

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,23 +1,70 @@
-public class MainClass {
+import java.io.*;
+import java.net.Socket;
 
-    private static void oldness(Integer myInt)
-    {
-        boolean b = myInt > 0;
-        if (!b) {
-            System.out.println("Input must be positive");
-        }
-        else {
-            System.out.print("Age in days = " + (int)(myInt * 365.25));
-        }
-    }
-    public static void main(String args[]) {
+public class MainClass{
 
+    private static Socket socket;
+    private final static int DEFAULT_PORT = 79;
+    private final static int BUFF_SIZE = 1024;
+    private final static String HOST = "hermes.cam.ac.uk";
+
+    private static void initSocket(){
         try {
-            Integer i = Integer.parseInt(args[0]);
-            oldness(i);
+            System.out.println("Connecting...");
+            socket = new Socket(HOST, DEFAULT_PORT);
+            System.out.println("Successfully connected to " + HOST + " on port " + DEFAULT_PORT + "!");
         }
-        catch (ArrayIndexOutOfBoundsException | NumberFormatException e){
+        catch (IOException e){
             e.printStackTrace();
         }
     }
+
+    private static class fingerServer extends Thread{
+        @Override
+        public void run(){
+            try {
+                char[] buffer = new char[BUFF_SIZE];
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                int bytesRead = in.read(buffer, 0, BUFF_SIZE);
+
+                if (bytesRead >= 0) {
+                    System.out.println(new String(buffer, 0, bytesRead));
+                }
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void runServer(){
+        Thread fingerServer = new fingerServer();
+        fingerServer.start();
+    }
+
+    private static void getFinger() throws IOException {
+
+        initSocket();
+
+        BufferedWriter w;
+        BufferedReader r;
+
+        w = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        r = new BufferedReader(new InputStreamReader(System.in));
+
+        runServer();
+
+        w.write(r.readLine() + "\r\n");
+        w.flush();
+    }
+
+    public static void main(String[] args) {
+        try{
+            getFinger();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
